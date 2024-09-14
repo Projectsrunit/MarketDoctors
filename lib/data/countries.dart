5fb5ef0db7c81bfd66395d43297103e7e6ccedf3
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-09-14 12:12:16 +0300

    countries input

---
+++ lib/data/countries.dart
@@ -0,0 +1,8 @@
+final List<Map<String, String>> countryCodes = [
+  {'name': 'Nigeria', 'code': '+234', 'flag': 'ng'},
+  {'name': 'Kenya', 'code': '+254', 'flag': 'ke'},
+  {'name': 'Uganda', 'code': '+256', 'flag': 'ug'},
+  {'name': 'Ghana', 'code': '+233', 'flag': 'gh'},
+  {'name': 'South Africa', 'code': '+27', 'flag': 'za'},
+  {'name': 'Andorra', 'code': '+376', 'flag': 'ad'},  // Example for +376
+];
