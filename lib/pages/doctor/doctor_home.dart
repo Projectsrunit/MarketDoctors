f2a9dbb875ab03e8f07dcb42cb480c8157c2ea52
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-09-16 22:59:08 +0300

    appointment cards

--- lib/pages/doctor/doctor_home.dart
+++ lib/pages/doctor/doctor_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_doctor/pages/chew/bottom_nav_bar.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -63,153 +64,192 @@ class _DashboardPageState extends State<DashboardPage> {
           ),
         ),
       ),
-      bottomNavigationBar: _buildBottomNavigation(),
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 
   Widget _buildSearchbar() {
-  return Row(
-    children: [
-      // Search bar with shadow
-      Expanded(
-        child: Container(
+    return Row(
+      children: [
+        // Search bar with shadow
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search Cases, Appointment, Pharmacy',
+                hintStyle:
+                    TextStyle(color: Colors.grey[500]), // Subtle hint color
+                prefixIcon: const Icon(Icons.search,
+                    color: Colors.black), // Search icon inside
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 12.0), // Comfortable padding
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Container(
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.3),
                 spreadRadius: 2,
                 blurRadius: 5,
-                offset: const Offset(0, 2),
+                offset: const Offset(0, 2), // Shadow position
               ),
             ],
+            borderRadius: BorderRadius.circular(
+                10), // Matching the button's border radius
           ),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Search Cases, Appointment, Pharmacy',
-              hintStyle: TextStyle(color: Colors.grey[500]), // Subtle hint color
-              prefixIcon: const Icon(Icons.search, color: Colors.black), // Search icon inside
-              contentPadding: const EdgeInsets.symmetric(vertical: 12.0), // Comfortable padding
-              border: InputBorder.none, 
+          child: TextButton.icon(
+            onPressed: () {
+              // Handle filter action
+            },
+            icon: const Icon(Icons.filter_list,
+                color: Colors.black), // Filter icon
+            label: const Text(
+              'Filter',
+              style:
+                  TextStyle(color: Colors.black), // Text for the filter button
+            ),
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 15.0, vertical: 12.0), // Consistent padding
+              backgroundColor: Colors
+                  .white, // White background for consistency with search bar
+              foregroundColor: Colors.black, // Black text/icon color
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // Rounded corners
+              ),
             ),
           ),
-        ),
-      ),
-      const SizedBox(width: 10), 
-      Container(
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 2), // Shadow position
-            ),
-          ],
-          borderRadius: BorderRadius.circular(10), // Matching the button's border radius
-        ),
-        child: TextButton.icon(
-          onPressed: () {
-            // Handle filter action
-          },
-          icon: const Icon(Icons.filter_list, color: Colors.black), // Filter icon
-          label: const Text(
-            'Filter',
-            style: TextStyle(color: Colors.black), // Text for the filter button
-          ),
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12.0), // Consistent padding
-            backgroundColor: Colors.white, // White background for consistency with search bar
-            foregroundColor: Colors.black, // Black text/icon color
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10), // Rounded corners
-            ),
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
-
-  Widget _buildDashboardCards() {
-    return GridView.count(
-      shrinkWrap: true,
-      crossAxisCount: 2,
-      crossAxisSpacing: 16,
-      mainAxisSpacing: 16,
-      children: [
-        _buildDashboardCard(
-          icon: Icons.cases,
-          label: 'Cases',
-          onTap: () {}, // Replace with your cases action
-        ),
-        _buildDashboardCard(
-          icon: Icons.local_pharmacy,
-          label: 'Pharmacy',
-          onTap: () {}, // Replace with your pharmacy action
-        ),
-        _buildDashboardCard(
-          icon: Icons.calendar_today,
-          label: 'Appointment',
-          onTap: () {}, // Replace with your appointment action
-        ),
-        _buildDashboardCard(
-          icon: Icons.payment,
-          label: 'Payment',
-          onTap: () {}, // Replace with your payment action
         ),
       ],
     );
   }
 
-  Widget _buildDashboardCard({
-    required IconData icon,
+  Widget _buildDashboardCards() {
+    const SizedBox(height: 15);
+
+    return GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: 3,
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      children: [
+        _buildDashboardCardWithLabel(
+          image: AssetImage('assets/images/cases-image.png'),
+          label: 'Cases',
+          onTap: () {},
+        ),
+        _buildDashboardCardWithLabel(
+          image: AssetImage('assets/images/pills-image.png'),
+          label: 'Pharmacy',
+          onTap: () {},
+        ),
+        _buildDashboardCardWithLabel(
+          icon: Icons.person_add_alt_1_outlined,
+          label: 'Appointment',
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDashboardCardWithLabel({
+    AssetImage? image,
+    IconData? icon,
     required String label,
     required VoidCallback onTap,
   }) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.blueAccent.withOpacity(0.2),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 40, color: Colors.blueAccent),
-            const SizedBox(height: 8),
-            Text(label, style: const TextStyle(fontWeight: FontWeight.bold)),
-          ],
-        ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              height: 250,
+              decoration: BoxDecoration(
+                color: Color(0xFF617DEF).withOpacity(0.2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: image != null
+                    ? Image(image: image, fit: BoxFit.contain)
+                    : Icon(icon, size: 40, color: Color(0xFF617DEF)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildBanner() {
     return Container(
-      height: 150,
+      height: 120,
+      margin: const EdgeInsets.symmetric(vertical: 16.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        image: const DecorationImage(
-          image: AssetImage('assets/images/doctor-image.png'),
-          fit: BoxFit.cover,
-        ),
+        color: Color(0xFF617DEF).withOpacity(0.5),
       ),
-      child: Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.black.withOpacity(0.5),
-        ),
-        child: const Text(
-          'Discover the Secret to the Perfect Smile\n"Whitegate"',
-          style: TextStyle(
-              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Thought about secret \n to having the perfect \n smile..... “Whitegate”',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ),
+          // Image section on the right
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/advert-image.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -218,21 +258,52 @@ class _DashboardPageState extends State<DashboardPage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Next Appointment',
-            style: TextStyle(fontWeight: FontWeight.bold)),
-        const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            _buildAppointmentCard(
-              icon: Icons.videocam,
+            const Text(
+              'Next Appointment',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            GestureDetector(
+              onTap: () {}, // Replace with your "See all" action
+              child: const Text(
+                'See all',
+                style: TextStyle(
+                  color: Color(0xFF4672ff),
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16), 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildSmallAppointmentCard(
+              time: '02:00 PM',
+              date: 'April 2, 2024',
+            ),
+            _buildSmallAppointmentCard(
+              time: '02:00 PM',
+              date: 'April 2, 2024',
+            ),
+          ],
+        ),
+        const SizedBox(height: 16), 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildLargeAppointmentCard(
               label: 'Video Consultation',
               doctorName: 'Dr. Goodness Usorah',
               time: '02:00 PM',
               date: 'April 2, 2024',
             ),
-            _buildAppointmentCard(
-              icon: Icons.mic,
+            const SizedBox(width: 8),
+            _buildLargeAppointmentCard(
               label: 'Audio Consultation',
               doctorName: 'Dr. John Ogundipe',
               time: '02:00 PM',
@@ -240,72 +311,92 @@ class _DashboardPageState extends State<DashboardPage> {
             ),
           ],
         ),
-        const SizedBox(height: 8),
-        TextButton(
-          onPressed: () {}, // Replace with your "See all" action
-          child: const Text('See all'),
-        ),
       ],
     );
   }
 
-  Widget _buildAppointmentCard({
-    required IconData icon,
+  Widget _buildSmallAppointmentCard({
+    required String time,
+    required String date,
+  }) {
+    return Container(
+      width: 120,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color(0xFF617DEF),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$time, $date',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLargeAppointmentCard({
     required String label,
     required String doctorName,
     required String time,
     required String date,
   }) {
     return Container(
-      width: 150,
-      height: 100,
+      width: 170,
+      height: 120,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[200],
+        color: Color(0xFF617DEF),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: const EdgeInsets.all(12.0),
+      
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Icon(icon, color: Colors.blueAccent),
-          const SizedBox(height: 4),
-          Text(label),
-          const SizedBox(height: 4),
-          Text('$doctorName'),
-          const SizedBox(height: 4),
-          Text('$time, $date'),
+          // White line on the left
+          Container(
+            width: 4, // Increased thickness of the white line
+            height: 45, // Full height of the card
+            color: Colors.white,
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+             
+                const SizedBox(height: 8),
+                Text(
+                  label,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  doctorName,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
-    );
-  }
-
-  Widget _buildBottomNavigation() {
-    return BottomNavigationBar(
-      currentIndex: _currentIndex,
-      selectedItemColor: Colors.blueAccent,
-      unselectedItemColor: Colors.grey,
-      onTap: (index) {
-        setState(() {
-          _currentIndex = index;
-        });
-      },
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.cases),
-          label: 'Cases',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.payment),
-          label: 'Payment',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
     );
   }
 }
