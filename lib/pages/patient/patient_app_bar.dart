e125591bb1a3d54b06c2dc8723ac7b97d6ce9197
Arafat Magezi <arafats144@gmail.com>
2024-09-21 00:12:21 +0300

    Patient Dashboard5

--- lib/pages/patient/patient_app_bar.dart
+++ lib/pages/patient/patient_app_bar.dart
@@ -7,28 +7,38 @@ AppBar patientAppBar(String patientName) {
       padding: const EdgeInsets.all(8.0),
       child: Row(
         children: [
-          CircleAvatar(
+          const CircleAvatar(
             backgroundColor: Colors.white,
             child: Icon(Icons.person),
           ),
-          SizedBox(width: 6),
+          const SizedBox(width: 6),
           Flexible(
-            child: Text(
-              patientName, // Use the patient name here
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Hi, ',  // The "Hi," part
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      fontSize: 18,
+                    ),
+                  ),
+                  TextSpan(
+                    text: patientName,  // The patient's name
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
               overflow: TextOverflow.ellipsis,
             ),
           ),
         ],
       ),
     ),
-    actions: [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: IconButton(
-          icon: Icon(Icons.notifications),
-          onPressed: () {},
-        ),
-      ),
-    ],
   );
 }
