ba3319dbf1f31e1826d88ef07b56fb402a25f0ac
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-09-22 13:49:50 +0300

    Add Appointment page

--- lib/pages/doctor/doctor_appointment.dart
+++ lib/pages/doctor/doctor_appointment.dart
@@ -2,139 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:market_doctor/pages/doctor/bottom_nav_bar.dart';
 import 'package:market_doctor/pages/doctor/doctor_appbar.dart';
-import 'package:market_doctor/pages/doctor/doctor_appointment.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class DoctorFormPage extends StatefulWidget {
-  const DoctorFormPage({Key? key}) : super(key: key);
+class DoctorAppointmentPage extends StatefulWidget {
+  const DoctorAppointmentPage({Key? key}) : super(key: key);
 
   @override
-  State<DoctorFormPage> createState() => _DoctorFormPageState();
+  State<DoctorAppointmentPage> createState() => _DoctorAppointmentPageState();
 }
 
-class _DoctorFormPageState extends State<DoctorFormPage> {
+class _DoctorAppointmentPageState extends State<DoctorAppointmentPage> {
   final _formKey = GlobalKey<FormState>();
-  final _yearsOfExperienceController = TextEditingController();
-  final _clinicHealthFacilityController = TextEditingController();
-  final _specializationController = TextEditingController();
-  final _awardsAndRecognitionController = TextEditingController();
-  final _languageController = TextEditingController();
-
+  final _noteController = TextEditingController();
   DateTime? _selectedDate;
   List<String> _selectedTimeSlots = [];
 
-  // Function to open the date picker
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-    if (pickedDate != null && pickedDate != _selectedDate) {
-      setState(() {
-        _selectedDate = pickedDate;
-        _selectedTimeSlots
-            .clear(); // Reset selected time slots for the new date
-      });
-    }
+  @override
+  void dispose() {
+    _noteController.dispose();
+    super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: doctorAppBar(),
-      body: SingleChildScrollView(
+  Widget _buildDoctorInfoCard() {
+    return Card(
+      child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  'Doctor Form',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/doctor-image.png'),
                 ),
-              ),
-              const SizedBox(height: 20),
-              _buildLabeledTextField(
-                controller: _yearsOfExperienceController,
-                labelText: 'Years of Experience',
-              ),
-              const SizedBox(height: 16),
-              _buildLabeledTextField(
-                controller: _clinicHealthFacilityController,
-                labelText: 'Clinic / Health Facility',
-              ),
-              const SizedBox(height: 16),
-              _buildLabeledTextField(
-                controller: _specializationController,
-                labelText: 'Specialization',
-              ),
-              const SizedBox(height: 20),
-              _buildCalendar(context),
-              if (_selectedDate != null) ...[
-                const SizedBox(height: 20),
-                _buildTimeSlots(), // Show time slots after date selection
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Dr. Samuel Jason',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('Surgeon'),
+                    Text('Available: 24 Hours'),
+                  ],
+                ),
               ],
-              const SizedBox(height: 20),
-              _buildLabeledTextField(
-                controller: _languageController,
-                labelText: 'Languages',
-              ),
-              const SizedBox(height: 16),
-              _buildLabeledTextField(
-                controller: _awardsAndRecognitionController,
-                labelText: 'Awards & Recognition',
-              ),
-              const SizedBox(height: 30),
-              _buildSaveButton(),
-            ],
-          ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.call),
+                  onPressed: () {
+                    // Handle phone call action
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.videocam),
+                  onPressed: () {
+                    // Handle video call action
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.message),
+                  onPressed: () {
+                    // Handle messaging action
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-      bottomNavigationBar: DoctorBottomNavBar(),
     );
   }
 
-  Widget _buildLabeledTextField({
-    required TextEditingController controller,
-    required String labelText,
-  }) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        // Label on the left
-        SizedBox(
-          width: 150,
-          child: Text(
-            labelText,
-            style: const TextStyle(fontSize: 16, color: Colors.black87),
-          ),
-        ),
-        // Input field on the right
-        Expanded(
-          child: TextFormField(
-            controller: controller,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  // Build the calendar widget
-  Widget _buildCalendar(BuildContext context) {
+  Widget _buildCalendarSection(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -205,7 +153,6 @@ class _DoctorFormPageState extends State<DoctorFormPage> {
     );
   }
 
-  // Build the time slots widget
   Widget _buildTimeSlots() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -254,29 +201,82 @@ class _DoctorFormPageState extends State<DoctorFormPage> {
     );
   }
 
-  Widget _buildSaveButton() {
-    return Align(
-      alignment: Alignment.centerRight,
+  Widget _buildNoteSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Note',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8),
+        TextField(
+          controller: _noteController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter additional notes for your appointment',
+          ),
+          maxLines: 4,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAppointmentButton() {
+    return SizedBox(
+      width: double.infinity, // Full-width button
       child: ElevatedButton(
         onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const DoctorAppointmentPage(),
-            ),
-          );
+          if (_selectedDate != null) {
+            // Handle appointment scheduling logic here
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Please select a date')),
+            );
+          }
         },
         style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
           backgroundColor: Colors.blueAccent,
         ),
         child: const Text(
-          'Save',
+          'Make an Appointment',
           style: TextStyle(fontSize: 18, color: Colors.white),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: doctorAppBar(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildDoctorInfoCard(),
+              const SizedBox(height: 16),
+              _buildCalendarSection(context),
+              if (_selectedDate != null) ...[
+                const SizedBox(height: 20),
+                _buildTimeSlots(), // Show time slots after date selection
+              ],
+              const SizedBox(height: 16),
+              _buildNoteSection(),
+              const SizedBox(height: 32),
+              _buildAppointmentButton(),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: DoctorBottomNavBar(),
     );
   }
 }
