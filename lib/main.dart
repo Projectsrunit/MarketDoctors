37aa12ea6cdef294bae212e0c87a5946a437c887
Arafat Magezi <arafats144@gmail.com>
2024-10-06 22:30:04 +0300

    Added auto focus to the otp field

--- lib/main.dart
+++ lib/main.dart
@@ -96,8 +96,7 @@ class MyApp extends StatelessWidget {
       ),
       themeMode: themeNotifier.themeMode,
       // home: ChooseUserTypePage(),
-      // home: OnboardingScreen()
-      home: PatientVerificationPage(reference: '',)
+      home: OnboardingScreen()
     );
   }
 }
