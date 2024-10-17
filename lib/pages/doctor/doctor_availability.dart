c18b2bd8e6826acc272f3ec51b02056d5c429b57
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-10-17 16:44:38 +0300

    Delete availability

--- lib/pages/doctor/doctor_availability.dart
+++ lib/pages/doctor/doctor_availability.dart
@@ -4,7 +4,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:market_doctor/main.dart';
 import 'package:market_doctor/pages/doctor/bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
 class DoctorAvailability extends StatefulWidget {
@@ -36,13 +35,10 @@ class _DoctorAvailabilityState extends State<DoctorAvailability> {
     String? baseUrl = dotenv.env['API_URL'];
 
     final String apiUrl =
-        "$baseUrl/api/availabilities?filters[users_permissions_user][id]=${doctorData?['id']}&populate=*'";
-    // final String token = Provider.of<DataStore>(context, listen: false).authToken;
+        "$baseUrl/api/availabilities?filters[users_permissions_user][id]=${doctorData?['id']}&populate=*";
 
     try {
-      final response = await http.get(
-        Uri.parse(apiUrl),
-      );
+      final response = await http.get(Uri.parse(apiUrl));
 
       if (response.statusCode == 200) {
         final responseData = json.decode(response.body);
@@ -67,6 +63,73 @@ class _DoctorAvailabilityState extends State<DoctorAvailability> {
     }
   }
 
+  // Update availability
+  // Function to update availability
+  Future<void> _updateAvailability(String appointmentId, String newDate,
+      String startTime, String endTime) async {
+    String? baseUrl = dotenv.env['API_URL'];
+    final String apiUrl =
+        "$baseUrl/api/availabilities/${appointmentId.toString()}"; // Ensure ID is a string
+
+    final Map<String, dynamic> body = {
+      "data": {
+        "date": newDate,
+        "available_time": [
+          {
+            "start_time": startTime,
+            "end_time": endTime,
+          }
+        ]
+      }
+    };
+
+    try {
+      final response = await http.put(
+        Uri.parse(apiUrl),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(body),
+      );
+
+      if (response.statusCode == 200) {
+        _showSnackBar('Availability updated successfully.');
+        _fetchAvailabilities(); // Refresh the list
+      } else {
+        _showSnackBar('Failed to update availability.');
+        print('Response: ${response.body}');
+      }
+    } catch (error) {
+      _showSnackBar('Error updating availability: $error');
+    }
+  }
+
+  // Delete availability
+// Delete availability
+  Future<void> _deleteAvailability(String appointmentId) async {
+    String? baseUrl = dotenv.env['API_URL'];
+
+    final String apiUrl =
+        "$baseUrl/api/availabilities/${appointmentId.toString()}"; 
+    print('AppointmentId $appointmentId');
+
+    try {
+      final response = await http.delete(
+        Uri.parse(apiUrl),
+      );
+
+      if (response.statusCode == 204 || response.statusCode == 200) {
+        _showSnackBar('Availability deleted successfully.');
+        _fetchAvailabilities(); // Refresh the list
+      } else {
+        _showSnackBar('Failed to delete availability.');
+        print('Response: ${response.statusCode} ${response.body}');
+      }
+    } catch (error) {
+      _showSnackBar('Error deleting availability: $error');
+    }
+  }
+
   // Show a snackbar for error messages
   void _showSnackBar(String message) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -75,7 +138,91 @@ class _DoctorAvailabilityState extends State<DoctorAvailability> {
     ));
   }
 
-  @override
+  // Show dialog for updating availability
+  void _showUpdateDialog(String appointmentId, String currentDate,
+      String currentStartTime, String currentEndTime) {
+    TextEditingController dateController =
+        TextEditingController(text: currentDate);
+    TextEditingController startTimeController =
+        TextEditingController(text: currentStartTime);
+    TextEditingController endTimeController =
+        TextEditingController(text: currentEndTime);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Update Availability'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: dateController,
+                decoration: InputDecoration(labelText: 'Date (YYYY-MM-DD)'),
+              ),
+              TextField(
+                controller: startTimeController,
+                decoration: InputDecoration(labelText: 'Start Time (HH:mm:ss)'),
+              ),
+              TextField(
+                controller: endTimeController,
+                decoration: InputDecoration(labelText: 'End Time (HH:mm:ss)'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _updateAvailability(
+                  appointmentId,
+                  dateController.text,
+                  startTimeController.text,
+                  endTimeController.text,
+                );
+                Navigator.of(context).pop();
+              },
+              child: Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Confirm delete action
+  void _confirmDelete(String appointmentId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Availability'),
+          content: Text('Are you sure you want to delete this availability?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteAvailability(appointmentId);
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,7 +236,7 @@ class _DoctorAvailabilityState extends State<DoctorAvailability> {
                       itemCount: availabilities.length,
                       itemBuilder: (context, index) {
                         final availability = availabilities[index];
-                        // Null-check on 'availability' and nested objects
+                        final appointmentId = availability['id'];
                         final date = availability['attributes']?['date'] ??
                             'Unknown Date';
                         final availableTime =
@@ -101,51 +248,64 @@ class _DoctorAvailabilityState extends State<DoctorAvailability> {
                             ? availableTime[0]['end_time']
                             : 'N/A';
 
-                        return ListTile(
-                          title: Text('Date: $date'),
-                          subtitle: Text('Time: $startTime - $endTime'),
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          child: Card(
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Date: $date',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Text(
+                                    'Time: $startTime - $endTime',
+                                    style: TextStyle(fontSize: 14.0),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          _showUpdateDialog(
+                                              appointmentId.toString(),
+                                              date,
+                                              startTime,
+                                              endTime);
+                                        },
+                                        child: Text('Edit Availability'),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(Icons.delete,
+                                            color: Colors.red),
+                                        onPressed: () {
+                                          _confirmDelete(
+                                              appointmentId.toString());
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         );
                       },
                     )
                   : Center(child: Text('No availabilities found.')),
-    );
-  }
-
-  // Build list of availabilities
-  Widget _buildAvailabilityList() {
-    if (availabilities.isEmpty) {
-      return Center(child: Text('No availabilities found.'));
-    }
-
-    return ListView.builder(
-      itemCount: availabilities.length,
-      itemBuilder: (context, index) {
-        final availability = availabilities[index]['attributes'];
-        final doctor =
-            availability['users_permissions_user']['data']['attributes'];
-
-        return Card(
-          child: ListTile(
-            title: Text(
-              '${doctor['firstName']} ${doctor['lastName']} - ${doctor['specialisation']}',
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Date: ${availability['date']}'),
-                Text(
-                    'Available from ${availability['available_time'][0]['start_time']} to ${availability['available_time'][0]['end_time']}'),
-                Text('Facility: ${doctor['facility']}'),
-              ],
-            ),
-            leading: doctor['profile_picture'] != null
-                ? CircleAvatar(
-                    backgroundImage: NetworkImage(doctor['profile_picture']),
-                  )
-                : CircleAvatar(child: Icon(Icons.person)),
-          ),
-        );
-      },
     );
   }
 }
