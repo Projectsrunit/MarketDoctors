928f9677d1b6647062c2d61dd0b267bea4a352a0
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2025-01-22 16:53:38 +0300

    Refactor: Doctor list

--- lib/pages/patient/doctor_card.dart
+++ lib/pages/patient/doctor_card.dart
@@ -1,106 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:market_doctor/chat_store.dart';
-import 'dart:convert';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class DoctorCardScreen extends StatefulWidget {
-  @override
-  _DoctorCardScreenState createState() => _DoctorCardScreenState();
-}
-
-class _DoctorCardScreenState extends State<DoctorCardScreen> {
-  List<dynamic> doctorData = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchDoctorData();
-  }
-
-  Future<void> fetchDoctorData() async {
-    final response = await http.get(Uri.parse(
-        '{{BASE_URL}}/api/users?filters[id][\$eq]=35&filters[role][\$eq]=3&populate=*'));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> responseData = json.decode(response.body);
-      setState(() {
-        doctorData = responseData;
-      });
-    } else {
-      throw Exception('Failed to load doctor data');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Doctors'),
-      ),
-      body: doctorData.isNotEmpty
-          ? ListView.builder(
-              itemCount: doctorData.length,
-              itemBuilder: (context, index) {
-                final doctor = doctorData[index];
-
-                // Extract the profile picture URL
-                final profilePictureUrl = doctor['profile_picture']?['formats']
-                        ?['thumbnail']?['url'] ??
-                    '';
-                final fullName = '${doctor['firstName']} ${doctor['lastName']}';
-
-                // Access the specialisation field
-                final profession = doctor['specialisation'] != null &&
-                        doctor['specialisation'].isNotEmpty
-                    ? doctor['specialisation']
-                    : 'Not available';
-// Fallback if specialisation is empty or null
-
-                print('Specialisation: $profession');
-
-                const rating = 4.5; // Assuming static rating for now
-
-                // Construct the full URL using {BASE_URL}
-                final imageUrl = '{{BASE_URL}}$profilePictureUrl';
-
-                return DoctorCard(
-                  id: -1,
-                  imageUrl: imageUrl,
-                  name: fullName,
-                  profession: profession,
-                  rating: rating,
-                  onChatPressed: () {
-                    // Handle chat press
-                  },
-                  onViewProfilePressed: () {
-                    // Handle view profile press
-                  },
-                  onBookAppointmentPressed: () {
-                    // Handle book appointment press
-                  },
-                );
-              },
-            )
-          : Center(child: CircularProgressIndicator()),
-    );
-  }
-}
 
 class DoctorCard extends StatelessWidget {
   final String imageUrl;
   final String name;
-  final String profession;
   final int id;
+  final String profession;
   final double rating;
   final VoidCallback onChatPressed;
   final VoidCallback onViewProfilePressed;
   final VoidCallback onBookAppointmentPressed;
 
   DoctorCard({
-    required this.imageUrl,
     required this.id,
+    required this.imageUrl,
     required this.name,
     required this.profession,
     required this.rating,
@@ -111,179 +25,163 @@ class DoctorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final unreadList =
-        context.read<ChatStore>().idsWithUnreadMessages;
+    final unreadList = context.read<ChatStore>().idsWithUnreadMessages;
     return Container(
-      // margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      margin: EdgeInsets.only(top: 0, bottom: 8, right: 5, left: 5),
-      decoration: BoxDecoration(
-        color: Colors.white, // Card background color
-        borderRadius: BorderRadius.circular(8), // Rounded corners
-        border: Border.all(
-          color: Colors.grey.shade300, // Border color
-          width: 1, // Border thickness
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3), // Shadow color
-            spreadRadius: 2, // Spread of shadow
-            blurRadius: 5, // Blur radius for smooth shadow
-            offset: Offset(0, 3), // Shadow position (x, y)
+        margin: EdgeInsets.only(top: 0, bottom: 8, right: 5, left: 5),
+        decoration: BoxDecoration(
+          color: Theme.of(context).brightness == Brightness.dark
+              ? Colors.grey[800]
+              : Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.grey[800]!
+                : Colors.grey.shade300,
+            width: 1,
           ),
-        ],
-      ),
-      child: Container(
-        height: 145,
-        padding: const EdgeInsets.all(2.0),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Image.network(
-                imageUrl,
-                width: 112,
-                height: 120,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(width: 4),
-            Flex(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              direction: Axis.vertical,
-              children: [
-                Text(
-                  name,
-                  style: GoogleFonts.nunito(
-                    textStyle: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3)),
+          ],
+        ),
+        child: Container(
+          height: 145,
+          padding: const EdgeInsets.all(2.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Image.network(
+                  imageUrl,
+                  width: 112,
+                  height: 120,
+                  fit: BoxFit.cover,
                 ),
-                Text(profession),
-                GestureDetector(
-                  onTap: onViewProfilePressed,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
-                    child: Text(
-                      'View Profile',
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                          fontSize: 13,
+              ),
+              SizedBox(width: 4),
+              Flex(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  direction: Axis.vertical,
+                  children: [
+                    Text(name,
+                        style: TextStyle(
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                        )),
+                    Text(profession),
+                    GestureDetector(
+                      onTap: onViewProfilePressed,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5)),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
+                        child: Text('View Profile',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Row(
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.amber, size: 16),
+                          SizedBox(width: 4.0),
+                          Text('$rating',
+                              style: TextStyle(
+                                  color: Colors.amber,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14)),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    GestureDetector(
+                      onTap: onChatPressed,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5)),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
+                        constraints: BoxConstraints(
+                          minHeight: 24,
+                        ),
+                        child: Text('Chat with doctor',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                      ),
+                    ),
+                    // SizedBox(height: 4,)
+                  ]),
+              SizedBox(width: 8.0),
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2),
+                  child: Flex(
+                    direction: Axis.vertical,
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Icon(Icons.star, color: Colors.amber, size: 16),
-                      SizedBox(width: 4.0),
-                      Text(
-                        '$rating',
-                        style: GoogleFonts.nunito(
-                          textStyle: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.amber,
+                      Consumer<ChatStore>(builder: (context, chatStore, child) {
+                        print(
+                            'Consumer builder called! this is the list: ${chatStore.idsWithUnreadMessages}');
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            if (unreadList.contains(id))
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 3.0, right: 3.0),
+                                child: Container(
+                                  width: 15,
+                                  height: 15,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                              )
+                          ],
+                        );
+                      }),
+                      Spacer(),
+                      GestureDetector(
+                        onTap: onBookAppointmentPressed,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 6.0, horizontal: 8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                    text: 'Book',
+                                    style: TextStyle(color: Colors.white)),
+                              ],
+                            ),
                           ),
                         ),
                       ),
+                      SizedBox(height: 4),
                     ],
                   ),
                 ),
-                SizedBox(height: 8),
-                GestureDetector(
-                  onTap: onChatPressed,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
-                    child: Text(
-                      'Chat with doctor',
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(width: 8.0),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2),
-                child: Flex(
-                  direction: Axis.vertical,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Consumer<ChatStore>(builder: (context, chatStore, child) {
-                      print(
-                          'Consumer builder called! this is the list: ${chatStore.idsWithUnreadMessages}');
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          if (unreadList.contains(id))
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 3.0, right: 3.0),
-                              child: Container(
-                                width: 15,
-                                height: 15,
-                                decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  shape: BoxShape.circle,
-                                ),
-                              ),
-                            )
-                        ],
-                      );
-                    }),
-                    Spacer(),
-                    GestureDetector(
-                      onTap: onBookAppointmentPressed,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 6.0, horizontal: 8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                  text: 'Book',
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                  ],
-                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
