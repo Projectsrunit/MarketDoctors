8154cc1bbffee34d4ec96bab57dad55423fd363b
Arafat <sp@MacBook-Pro.local>
2025-02-26 15:18:21 +0300

    Push to windows

--- lib/pages/notifications/notifications_page.dart
+++ lib/pages/notifications/notifications_page.dart
@@ -6,34 +6,6 @@ import 'package:provider/provider.dart';
 import 'package:market_doctor/data_store.dart';
 import 'package:market_doctor/models/notification_item.dart';
 
-class NotificationItem {
-  final String title;
-  final String message;
-  final String timestamp;
-  final Map<String, dynamic>? data;
-
-  NotificationItem({
-    required this.title,
-    required this.message,
-    required this.timestamp,
-    this.data,
-  });
-
-  Map<String, dynamic> toJson() => {
-    'title': title,
-    'message': message,
-    'timestamp': timestamp,
-    'data': data,
-  };
-
-  factory NotificationItem.fromJson(Map<String, dynamic> json) => NotificationItem(
-    title: json['title'],
-    message: json['message'],
-    timestamp: json['timestamp'],
-    data: json['data'],
-  );
-}
-
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
 
